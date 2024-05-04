2f6633425502280bf137d71b68a59b0e278ef000
Marcel <unesl@student.kit.edu>
2024-05-04 17:00:59 +0200

    adjusted builder fileds to real_kitchen datag

--- kit_irl_real_kitchen/kit_irl_real_kitchen_dataset_builder.py
+++ kit_irl_real_kitchen/kit_irl_real_kitchen_dataset_builder.py
@@ -1,3 +1,5 @@
+import os
+import cv2
 from typing import Iterator, Tuple, Any
 
 import glob
@@ -26,22 +28,36 @@ class KitIrlRealKitchen(tfds.core.GeneratorBasedBuilder):
                 'steps': tfds.features.Dataset({
                     'observation': tfds.features.FeaturesDict({
                         'image': tfds.features.Image(
-                            shape=(64, 64, 3),
+                            shape=(250, 250, 3),
                             dtype=np.uint8,
-                            encoding_format='png',
+                            encoding_format='jpeg',
                             doc='Main camera RGB observation.',
                         ),
                         'wrist_image': tfds.features.Image(
-                            shape=(64, 64, 3),
+                            shape=(250, 250, 3),
                             dtype=np.uint8,
                             encoding_format='png',
                             doc='Wrist camera RGB observation.',
                         ),
-                        'state': tfds.features.Tensor(
-                            shape=(10,),
-                            dtype=np.float32,
-                            doc='Robot state, consists of [7x robot joint angles, '
-                                '2x gripper position, 1x door opening angle].',
+                        'joint_state': tfds.features.Tensor(
+                            shape=(7,),
+                            dtype=np.float64,
+                            doc='Robot joint state. Consists of [7x joint states]',
+                        ),
+                        'joint_state_velocity': tfds.features.Tensor(
+                            shape=(7,),
+                            dtype=np.float64,
+                            doc='Robot joint velocities. Consists of [7x joint velocities]',
+                        ),
+                        'end_effector_pos': tfds.features.Tensor(
+                            shape=(3,),
+                            dtype=np.float64,
+                            doc='Current End Effector position in Cartesian space',
+                        ),
+                        'end_effector_ori': tfds.features.Tensor(
+                            shape=(3,),
+                            dtype=np.float64,
+                            doc='Current End Effector orientation in Cartesian space',
                         )
                     }),
                     'action': tfds.features.Tensor(
@@ -113,47 +129,86 @@ class KitIrlRealKitchen(tfds.core.GeneratorBasedBuilder):
         # )
 
 def _parse_example(episode_path, embed=None):
-            # load raw data --> this should change for your dataset
-            data = np.load(episode_path, allow_pickle=True)     # this is a list of dicts in our case
-            print(data)
+    data = {}
+    path = os.path.join(episode_path, "*.pickle")
+    for file in glob.glob(path):
+        # Keys contained in .pickle:
+        # 'joint_state', 'joint_state_velocity', 'des_joint_state', 'des_joint_vel', 'end_effector_pos', 'end_effector_ori', 'des_gripper_width', 'delta_joint_state',
+        # 'delta_des_joint_state', 'delta_end_effector_pos', 'delta_end_effector_ori', 'language_description', 'traj_length'
+        pickle_file_path = os.path.join(episode_path, file)
+        data.update(np.load(pickle_file_path, allow_pickle=True))
+    trajectory_length = data["traj_length"]
+    cam1_path = os.path.join(episode_path, "cam_1")
+    cam2_path = os.path.join(episode_path, "cam_2")
+    cam1_image_vector = create_img_vector(cam1_path, trajectory_length)
+    cam2_image_vector = create_img_vector(cam2_path, trajectory_length)
+    data.update({'image': cam1_image_vector, 'wrist_image': cam2_image_vector})
+    # print(data.keys())
+    # print(data["image"][0].shape) # (250, 250, 3)
+    # print(data["wrist_image"][0].shape) # (250, 250, 3)
 
-            # assemble episode --> here we're assuming demos so we set reward to 1 at the end
-            episode = []
-            for i, step in enumerate(data):
-                # compute Kona language embedding
-                language_embedding = embed([step['language_instruction']])[0].numpy() if embed is not None else np.zeros(512)
+    episode = []
+    for i in range(trajectory_length):
+        # compute Kona language embedding
+        # language_embedding = embed([data['language_instruction']])[0].numpy() if embed is not None else np.zeros(512)
+        language_embedding = embed(data['language_description']).numpy() if embed is not None else [np.zeros(512)]
+        action = np.append(data['delta_end_effector_pos'][i], data['delta_end_effector_ori'][i], axis=0)
+        action = np.append(action, data['des_gripper_width'][i])
+        # action = data['delta_end_effector_pos'][i]
+        # action = action.append(data['delta_end_effector_ori'][i])
+        # action = action.append(data['des_gripper_width'][i])
 
-                episode.append({
-                    'observation': {
-                        'image': step['image'],
-                        'wrist_image': step['wrist_image'],
-                        'state': step['state'],
-                    },
-                    'action': step['action'],
-                    'discount': 1.0,
-                    'reward': float(i == (len(data) - 1)),
-                    'is_first': i == 0,
-                    'is_last': i == (len(data) - 1),
-                    'is_terminal': i == (len(data) - 1),
-                    'language_instruction': step['language_instruction'],
-                    'language_embedding': language_embedding,
-                })
+        episode.append({
+            'observation': {
+                'image': data['image'][i],
+                'wrist_image': data['wrist_image'][i],
+                'joint_state': data['joint_state'][i],
+                'joint_state_velocity': data['joint_state_velocity'][i],
+                'end_effector_pos': data['end_effector_pos'][i],
+                'end_effector_ori': data['end_effector_ori'][i],
+            },
+            'action': action,
+            'action_joint_state': data['des_joint_state'][i],
+            'action_joint_vel': data['des_joint_vel'][i],
+            'action_gripper_width': data['des_gripper_width'][i],
+            'delta_des_joint_state': data['delta_des_joint_state'][i],
+            'discount': 1.0,
+            'reward': float(i == (data['traj_length'] - 1)),
+            'is_first': i == 0,
+            'is_last': i == (data['traj_length'] - 1),
+            'is_terminal': i == (data['traj_length'] - 1),
+            'language_instruction': data['language_description'],
+            'language_embedding': language_embedding,
+        })
 
-            # create output data sample
-            sample = {
-                'steps': episode,
-                'episode_metadata': {
-                    'file_path': episode_path
-                }
-            }
+    # create output data sample
+    sample = {
+        'steps': episode,
+        'episode_metadata': {
+            'file_path': episode_path,
+            'traj_length': data['traj_length'],
+        }
+    }
 
-            # if you want to skip an example for whatever reason, simply return None
-            return episode_path, sample
+    # if you want to skip an example for whatever reason, simply return None
+    return episode_path, sample
+
+def create_img_vector(img_folder_path, trajectory_length):        
+    cam_list = []
+    cam_path_list = []
+    for index in range(trajectory_length):
+        frame_file_name = '{}.jpeg'.format(index)
+        cam_path_list.append(frame_file_name)
+        img_path = os.path.join(img_folder_path, frame_file_name)
+        img_array = cv2.imread(img_path)
+        cam_list.append(img_array)
+    return cam_list
 
 if __name__ == "__main__":
     data_path = "/home/marcelr/uha_test_policy/finetune_data/*"
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
     # create list of all examples
     episode_paths = glob.glob(data_path)
-    for sample in episode_paths:
-        print(sample + "/*")
-        _parse_example(sample + "/*")
+    for episode in episode_paths:
+        _, sample = _parse_example(episode, embed)
+        print(sample)
